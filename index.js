09172af35d1cf6dc0a2d11818ede8c686f4d37db
GeethaMiza <geethamiza@gmail.com>
2023-06-24 08:00:40 +0530

    Setting mongoose db, layouts and ejs templates

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express=require('express');
+const path=require('path');
 const port=8000;
+const db=require('./config/mongoose');
 const app=express();
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 app.listen(port,function(err){
     if(err) console.log("Error in running the server",err);
