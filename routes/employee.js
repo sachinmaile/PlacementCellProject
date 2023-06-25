11d8ecfc7858b4e76a5083772b33f3a876e2a188
GeethaMiza <geethamiza@gmail.com>
2023-06-25 09:34:08 +0530

    Sample

--- routes/employee.js
+++ routes/employee.js
@@ -4,10 +4,11 @@ const empController=require('../controllers/emp_controller');
 const passport = require('passport');
 
 router.get('/signUp',empController.signUp);
+router.get('/signUp',empController.signUp);
 router.get('/signIn',empController.signIn);
 router.post('/create',empController.create);
 router.post('/create_session',passport.authenticate('local',{failureRedirect:'emp/signIn'}),empController.createSession);
 router.get('/profile',passport.checkAuthentication,empController.profile);
 router.get('/signOut',empController.destroySession);
 
-module.exports=router;
+module.exports=router;   
